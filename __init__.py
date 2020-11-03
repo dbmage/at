b73b648be6453a84b8b5e23ce0af750d8201cd66
dbmage <dbmage666@gmail.com>
2020-11-03 16:36:26 +0000

    [init] added author, license and logger handler

--- __init__.py
+++ __init__.py
@@ -6,10 +6,24 @@ if not (sys.version_info[0] >= 3):
 if sys.platform == 'win32':
     raise EnvironmentError("This module is not supported on Windows")
 
-from at import runOsCmd, getJobsList, addJob, addJobFromFile, removeJob, clearJobs
+from .at import runOsCmd, getJobsList, addJob, addJobFromFile, removeJob, clearJobs
+
+''' Set default logging handler to avoid "No handler found" warnings.
+Used code from requests 2.9.1, Copyright Kenneth Reitz'''
+import logging
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
 
 __version__ = '1.0.0'
 __revision__ = ''
+__author__ = 'Joe Ash (DBMage)'
+__license__ = 'Apache 2.0'
 __all__ = ['runOsCmd', 'getJobsList', 'addJob', 'addJobFromFile', 'removeJob', 'clearJobs', '__version__', '__revision__']
 
 
