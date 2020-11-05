edcdc4073680f5968e5b7a3662519b5e83992023
dbmage <dbmage666@gmail.com>
2020-11-05 15:54:33 +0000

    [at] fixed error handling and returning job id

--- at.py
+++ at.py
@@ -1,5 +1,6 @@
 ''' Wrapper around subprocess to allow easy use of AT command '''
 import os
+import re
 import sys
 import logging
 import subprocess
@@ -9,6 +10,7 @@ log = logging.getLogger(__name__)
 def runOsCmd(command,cmdin=None):
     if not isinstance(command, list):
         return False
+    jobidregex = re.compile(r'job ([0-9]+) at')
     try:
         out = subprocess.Popen(command,
                stdin=subprocess.PIPE,
@@ -16,14 +18,15 @@ def runOsCmd(command,cmdin=None):
                stderr=subprocess.PIPE)
         if cmdin != None:
             out.stdin.write(cmdin.encode())
-        output,errors = out.communicate()
-        if errors:
+        blank,output = out.communicate()
+        jobid = jobidregex.findall(output)
+        if jobid == None:
             cmdout = ' '.join(command)
             if cmdin:
                 cmdout += " %s" % (cmdin)
             log.error("Error occured running '%s': %s" % (cmdout, errors.decode('utf-8')))
             return False
-        return output.decode('utf-8')
+        return jobid
     except:
         return False
 
@@ -51,16 +54,16 @@ def getJobsList(queue='a'):
 
 def addJob(jobtime, queue, command):
     status = runOsCmd(['at', ':'.join(jobtime.split(':')[:2]), "-q%s" % (queue)], cmdin=command)
-    if isinstance(status, bool):
+    if status == False:
         return False
-    return status.split('\n')[1].split(' ')[1]
+    return status
 
 def addJobFromFile(jobtime, queue, file):
     filecontents = open(file).read()
     status = runOsCmd(['at', ':'.join(jobtime.split(':')[:2]), "-q%s" % (queue)], cmdin=filecontents)
-    if isinstance(status, bool):
+    if status == False:
         return False
-    return status.split('\n')[1].split(' ')[1]
+    return status
 
 def removeJob(jobid):
     if isinstance(jobid, int):
@@ -74,3 +77,4 @@ def clearJobs(queue):
     jobs = getJobsList(queue)
     for job in jobs:
         removeJob(job)
+    return True
